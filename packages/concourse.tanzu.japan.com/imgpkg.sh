db37aa5e011ea37390fa4e5d8f833180c6be6969
machih <machih@vmware.com>
2022-08-19 13:08:07 +0900

    tmp

--- packages/concourse.tanzu.japan.com/imgpkg.sh
+++ packages/concourse.tanzu.japan.com/imgpkg.sh
@@ -1,6 +1,6 @@
 mkdir -p imgpkg/.imgpkg
 helm template -n concourse -f helm-values.yaml concourse concourse/concourse > imgpkg/kubeconfig.yaml
 kbld -f imgpkg/ --imgpkg-lock-output imgpkg/.imgpkg/images.yml
-imgpkg copy --lock imgpkg/.imgpkg/images.yml --to-repo harbor.cl01.lespaulstudioplus.info/library/concourse --lock-output imgpkg/.imgpkg/images.yml
-imgpkg push -b harbor.cl01.lespaulstudioplus.info/pkgr/concourse-app:latest -f imgpkg/
+imgpkg copy --lock imgpkg/.imgpkg/images.yml --to-repo harbor.lespaulstudioplus.info/library/concourse --lock-output imgpkg/.imgpkg/images.yml
+imgpkg push -b harbor.lespaulstudioplus.info/pkgr/concourse-app:latest -f imgpkg/
 rm -rf imgpkg
